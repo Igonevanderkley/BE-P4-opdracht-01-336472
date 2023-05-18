14cd19927c45407220b1e8e25a42e0fc7f93ede2
Igone van_der_Kley <igone@vdkley.nl>
2023-05-18 09:23:36 +0200

    instructeur tabel

--- app/controllers/Untitled-1.php
+++ app/controllers/Untitled-1.php
@@ -67,39 +67,37 @@ class Instructeur extends BaseController
         $tableRows = '';
 
         foreach ($infoInstructeur as $info) {
-            $datum = date_create($info->Bouwjaar);
-            $datum = date_format($datum, 'd-m-Y'); 
             $tableRows .=  "<tr>
                                 <td>$info->typeVoertuig</td>
                                 <td>$info->Type</td>
                                 <td>$info->Kenteken</td>
-                                <td>$datum</td>
+                                <td>$info->Bouwjaar</td>
                                 <td>$info->Brandstof</td>
                                 <td>$info->RijbewijsCategorie</td>
                             </tr>";
         }
 
-        $naamDatumSterren = $this->instructeurModel->getLizhan();
+        $naamDatumSterren = $this->instructeurModel->getInstructeurs();
 
         $nds = '';
 
         foreach ($naamDatumSterren as $naam) {
             $datum = date_create($naam->DatumInDienst);
             $datum = date_format($datum, 'd-m-Y'); 
-            $aantalSterren =    $naam->AantalSterren;
-            $voor =             $naam->Voornaam;
-            $tussen =           $naam->Tussenvoegsel;
-            $achter =           $naam->Achternaam;
+            $aantalSterren = $naam->AantalSterren;
+            $voor = $naam->Voornaam;
+            $tussen = $naam->Tussenvoegsel;
+            $achter = $naam->Achternaam;
         }
 
         $data = [
-            'title'         => 'Door instructeur gebruikte voertuigen',
-            'tableRows'     => $tableRows,
-            'datum'         => $datum,
+            'title' => 'Door instructeur gebruikte voertuigen',
+            'tableRows' => $tableRows,
+            'datum' => $datum,
             'aantalSterren' => $aantalSterren,
-            'voor'          => $voor,
-            'tussen'        => $tussen, 
-            'achter'        => $achter
+            'voor' => $voor,
+            'tussen' => $tussen, 
+            'achter' => $achter
         ];
         $this->view('Instructeur/gebruikteVoertuigen', $data);
     }
---
+++ app/models/InstructeurModel.php
@@ -0,0 +1,49 @@
+<?php
+
+class InstructeurModel 
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getInstructeurs()
+    {
+        $sql = "SELECT * 
+                FROM instructeur 
+                ORDER BY AantalSterren DESC ";
+
+        $this->db->query($sql);
+        
+        return $this->db->resultSet();
+    }
+
+    public function getGegevensInstructeur()
+    {
+        $sql = "SELECT ty.typeVoertuig, vo.Type, vo.Kenteken, vo.Bouwjaar, vo.Brandstof, ty.RijbewijsCategorie
+                from instructeur ins
+                left join voertuiginstructeur voins
+                on ins.Id = voins.Id
+                left join voertuig vo
+                on vo.Id = ins.Id
+                left join typevoertuig ty
+                on ty.Id = vo.typevoertuigId
+                where ins.Id = 1";
+        
+        $this->db->query($sql);
+        
+        return $this->db->resultSet();
+    }
+
+    public function getLizhan(){
+        $sql = "SELECT * 
+        FROM instructeur
+        where Id = 1";
+
+        $this->db->query($sql);
+
+        return $this->db->resultSet();
+    }
+}
